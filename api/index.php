bb764e8c10f82fab2436a2dec65d3bd69d024bcf
Ralph Cajipe <ralphcajipe@gmail.com>
2022-06-01 06:30:01 +0800

    use an image as a link

--- api/index.php
+++ api/index.php
@@ -5,8 +5,7 @@
 -->
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Search</title>
 </head>
 
@@ -15,25 +14,24 @@
         <!-- Print a line of message that says "Search demo for English words". -->
         <h2>Search demo for English words</h2>
         <!-- Displaying the image of Bears searching. -->
-        <img alt="Bears searching" class="img-fluid w-25" src="/var/task/user/api/binocs.jpg">
+        <img alt="Bears searching" class="img-fluid w-25" src="https://m.media-amazon.com/images/M/MV5BNjhmMzVkOTktOThkMC00OTcyLTg4OWMtMTU1ZjZlN2RiZDM1L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDk2NDYyMTk@._V1_FMjpg_UX1000_.jpg">
 
         <!-- This is the form that the user will use to search for a query. -->
         <form action="" class="mt-4" method="get">
-            <input autocomplete="off" autofocus class="form-control form-control-lg mb-4 mx-auto w-50" name="word"
-                placeholder="Word" type="search">
+            <input autocomplete="off" autofocus class="form-control form-control-lg mb-4 mx-auto w-50" name="word" placeholder="Word" type="search">
             <button class="btn btn-light" type="submit">Search</button>
         </form>
 
     </div>
 
     <?php
-        // Including the RHCajipe_search.inc file. 
-        // `RHCajipe_search.inc` contains the code that checks whether the word is in the file `words.txt`.
-        
-        // This line makes the code in this file reusable, clean, and easy to read.
-        include("/var/task/user/api/RHCajipe_search.php");
-    
-        ?>
+    // Including the RHCajipe_search.inc file. 
+    // `RHCajipe_search.inc` contains the code that checks whether the word is in the file `words.txt`.
+
+    // This line makes the code in this file reusable, clean, and easy to read.
+    include("/var/task/user/api/RHCajipe_search.php");
+
+    ?>
 </body>
 
 </html>
